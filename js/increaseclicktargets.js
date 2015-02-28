cf8f951dce62c8f406182d1384320d78c7cf6826
bleistivt <bleistivt@users.noreply.github.com>
2015-02-28 10:06:10 +0100

    dry

--- js/increaseclicktargets.js
+++ js/increaseclicktargets.js
@@ -1,69 +1,52 @@
-$(function () {
+/*jslint browser: true */
+/*global jQuery*/
+
+jQuery.fn.increaseClickTarget = function (link, exclude, deferred) {
+    'use strict';
+
+    return this.on('click', deferred || '', function (e) {
+        var href = jQuery(this).find(link).attr('href');
+        if (!jQuery(e.target).is(exclude || '') && href !== undefined) {
+            document.location = href;
+        }
+    });
+
+};
+
+jQuery(function ($) {
     'use strict';
 
     // Discussions - Modern
-    $('ul.DataList.Discussions').on('click', 'li.Item', function (e) {
-        var href = $(this).find('.Title a').attr('href');
-        if (!$(e.target).is('span.OptionsTitle, a, a.Bookmark, input') && typeof href !== 'undefined') {
-            document.location = href;
-        }
-    });
+    $('ul.DataList.Discussions')
+        .increaseClickTarget('.Title a', 'span.OptionsTitle, a, a.Bookmark, input', 'li.Item');
     $('ul.DataList.Discussions li.Item').css('cursor', 'pointer');
 
     // Discussions - Table
-    $('table.DiscussionsTable').on('click', 'tr.Item', function (e) {
-        var href = $(this).find('a.Title').attr('href');
-        if (!$(e.target).is('span.OptionsTitle, a, a.Bookmark, input') && typeof href !== 'undefined') {
-            document.location = href;
-        }
-    });
+    $('table.DiscussionsTable')
+        .increaseClickTarget('a.Title', 'span.OptionsTitle, a, a.Bookmark, input', 'tr.Item');
     $('table.DiscussionsTable tr.Item').css('cursor', 'pointer');
 
     // Conversations
-    $('ul.DataList.Conversations').on('click', 'li.Item', function (e) {
-        var href = $(this).find('h3.Users>a').attr('href');
-        if (!$(e.target).is('span.OptionsTitle, a, a.Bookmark') && typeof href !== 'undefined') {
-            document.location = href;
-        }
-    });
+    $('ul.DataList.Conversations')
+        .increaseClickTarget('h3.Users>a', 'span.OptionsTitle, a, a.Bookmark', 'li.Item');
     $('ul.DataList.Conversations li.Item').css('cursor', 'pointer');
 
     // Categories - Modern
-    $('ul.DataList.CategoryList').on('click', 'li.Item', function (e) {
-        var href = $(this).find('a.Title').attr('href');
-        if (!$(e.target).is('span.OptionsTitle, a, input') && typeof href !== 'undefined') {
-            document.location = href;
-        }
-    });
+    $('ul.DataList.CategoryList')
+        .increaseClickTarget('a.Title', 'span.OptionsTitle, a, input', 'li.Item');
     $('ul.DataList.CategoryList li.Item').css('cursor', 'pointer');
 
     // Categories - Table
     $('table.CategoryTable tbody td.CategoryName')
-        .click(function (e) {
-            var href = $(this).find('h2 a').attr('href');
-            if (!$(e.target).is('span.OptionsTitle, a') && typeof href !== 'undefined') {
-                document.location = href;
-            }
-        })
+        .increaseClickTarget('h2 a', 'span.OptionsTitle, a')
         .css('cursor', 'pointer');
     $('table.CategoryTable tbody td.LatestPost')
-        .click(function () {
-            var href = $(this).find('a.BlockTitle').attr('href');
-            if (typeof href !== 'undefined') {
-                document.location = href;
-            }
-        })
+        .increaseClickTarget('a.BlockTitle')
         .css('cursor', 'pointer');
 
     // MeMenu
     $('div.MeMenu')
-        .on('click', 'li.Item', function () {
-            var href = $(this).find('a:last').attr('href');
-            if (typeof href !== 'undefined') {
-                document.location = href;
-                return false;
-            }
-        })
+        .increaseClickTarget('a:last', null, 'li.Item')
         .on('hover', 'li.Item', function () {
             $(this).css('cursor', 'pointer');
         });
