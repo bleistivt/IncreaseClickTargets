c0e706cae8ba2ddb90f1775999940d595fb4b02a
bleistivt <bleistivt@users.noreply.github.com>
2018-05-02 08:27:42 +0200

    remove plugininfo

--- class.increaseclicktargets.plugin.php
+++ class.increaseclicktargets.plugin.php
@@ -1,16 +1,5 @@
 <?php
 
-$PluginInfo['IncreaseClickTargets'] = [
-    'Name' => 'Increase Click Targets',
-    'Description' => 'Makes the whole row clickable for various items.',
-    'Version' => '0.4',
-    // The default mobile theme implements this already.
-    'MobileFriendly' => false,
-    'Author' => 'Bleistivt',
-    'AuthorUrl' => 'http://bleistivt.net',
-    'License' => 'GNU GPL2'
-];
-
 class IncreaseClickTargetsPlugin extends Gdn_Plugin {
 
     public function base_render_before($sender) {
